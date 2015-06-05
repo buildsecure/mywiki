3e7fd399dc1a35dddb641eedcd2c3d389c230666
Raymond Lee <ralee@lendinglcub.com>
2015-06-04 20:02:24 -0700

    commit tool1

--- src/tool1.java
+++ src/tool1.java
@@ -1,1 +1,19 @@
 test
+
+tomcat7:run
+
+Full name:
+
+org.apache.tomcat.maven:tomcat7-maven-plugin:2.0:run
+
+Description:
+Runs the current project as a dynamic web application using an embedded Tomcat server.
+
+Attributes:
+
+    Requires a Maven project to be executed.
+    Requires dependency resolution of artifacts in scope: test.
+    Since version: 2.0.
+    Invokes the execution of the lifecycle phase compile prior to executing itself.
+
+
